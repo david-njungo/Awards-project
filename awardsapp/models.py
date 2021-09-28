a69940109663c4d02ac364d3b25c1396a7d17802
David Njungo <davidnjungo3@gmail.com>
2021-09-28 15:29:57 +0300

    create image model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length =30)
+    link = models.CharField(max_length =30)
+    description = models.CharField(max_length =30)
+    # image
+    def __str__(self):
+        self.title
