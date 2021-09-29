876364fa94fd568278b424a06a7d2274735dcbe0
David Njungo <davidnjungo3@gmail.com>
2021-09-29 15:51:05 +0300

    add django registration and registration base template

--- awards/urls.py
+++ awards/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('awardsapp.urls'))
+    path('',include('awardsapp.urls')),
+    path('accounts/', include('registration.backends.simple.urls'))
 ]
