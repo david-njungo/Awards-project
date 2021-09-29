d5302cddac9f1cc74799453967f0262978d2b261
David Njungo <davidnjungo3@gmail.com>
2021-09-29 22:16:49 +0300

    add profile model

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Projects(models.Model):
@@ -24,3 +27,19 @@ class Projects(models.Model):
     def search_by_title(cls,search_term):
         projects = cls.objects.filter(title__icontains=search_term)
         return projects
+
+
+class Profile(models.Model):
+    prof_photo = models.ImageField(upload_to = 'profile/')
+    bio = models.TextField(max_length=500, blank=True)
+    contact = models.CharField(max_length=60,blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_profile_signal(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
